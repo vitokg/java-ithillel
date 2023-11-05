9c95fed0d4565e4366f46b42cd27731f3cd2831b
viktoriagoncharuk <viktoria.goncharuk@everything5pounds.com>
2023-11-05 14:14:12 +0100

    Homework1. Add convert miles to km method

--- Converter/src/Main.java
+++ Converter/src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
+        double resultImKm = convertMilesToKm(24);
+        printResult(resultImKm);
     }
 
+    public static final double KM_IN_ONE_MILE = 1.6;
+
+    public static double convertMilesToKm(double miles) {
+        return miles * KM_IN_ONE_MILE;
+    }
+
+    public static void printResult(double result) {
+        System.out.println(String.format("%.2f", result));
+    }
 }
